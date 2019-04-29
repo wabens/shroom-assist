aefaa2e03236c0375c9197f96262e140473d2e5b
wabens <waltkbenson@gmail.com>
2019-04-29 14:54:07 -0500

    select for target column in progress

--- src/components/TaskCreator/AddTarget.js
+++ src/components/TaskCreator/AddTarget.js
@@ -7,10 +7,15 @@ import { connect } from 'react-redux';
 class AddTarget extends Component {
 
     state= {
-        target_table: '',
-        modification_value: '',
-        modification: '',
+        target:{
+            target_table: '',
+            modification_value: {},
+            modification: '',
+            target_column:[],
+        },
         currentTable: [],
+        columnType: [],
+        targetColumnCount:0
     }
     
    handleChange = field => event => {
@@ -22,21 +27,39 @@ class AddTarget extends Component {
             currentTable = this.props.reduxState.processDataTypes.growingRoomTypes
        }
        this.setState({
-               ...this.state,
+           target:{
+               ...this.state.target,
                [field]: event.target.value,
-               currentTable,
+               },
+               currentTable
        })
-   }
+    };
+
+    handleColumnSelect = event => {
+        let column = event.target.value;
+        let targets = this.state.target.target_column;
+        targets.push(column.column_name)
+        console.log(`handleColumnSElect`, column, this.state.target);
+        
+        this.setState({
+            target:{
+                ...this.state.target,
+                target_column: targets
+            }
+        })
+    };
+
     render(){
         console.log(`in render `, this.state);
-        
+        let columnSelectEl=null;
         let tableEl = null;
+        let valueEl = null;
         let modificationEl =            
             <TextField
                 className={"formField"}
                 select
                 label="Type"
-                value={this.state.modification}
+                value={this.state.target.modification}
                 onChange={this.handleChange('modification')}
                 margin="dense"
                 variant="outlined"
@@ -44,27 +67,16 @@ class AddTarget extends Component {
                 <option value={'POST'}>Insert</option>
                 <option value={'PUT'}>Update</option>
             </TextField>;
-        let columnSelectEl = 
-            <TextField
-                className={"formField"}
-                select
-                label="Choose Column"
-                value={this.state.modification}
-                onChange={this.handleChange('target_column')}
-                margin="dense"
-                variant="outlined"
-            >
 
-            </TextField>;
         
-        if(this.state.modification.length){
+        if(this.state.target.modification.length){
             console.log(`addTarget modification`);
             tableEl =
                 <TextField
                     className={"formField"}
                     select
                     label="Table"
-                    value={this.state.target_table}
+                    value={this.state.target.target_table}
                     onChange={this.handleChange('target_table')}
                     margin="dense"
                     variant="outlined"
@@ -72,9 +84,25 @@ class AddTarget extends Component {
                     <option value={'incubator'}>incubator</option>
                     <option value={'growing_room'}>growing_room</option>
                 </TextField>;
-            if(this.state.modification === 'PUT'){
-                
-            } else if (this.state.modification === 'PUT') {
+            if(this.state.target.target_table.length && this.state.target.modification === 'PUT'){
+                columnSelectEl = 
+                    <TextField
+                        className={"formField"}
+                        select
+                        label="Column"
+                        value={this.state.target.target_column[this.state.targetColumnCount]||''}
+                        onChange={this.handleColumnSelect}
+                        margin="dense"
+                        variant="outlined"
+                    >
+                    {this.state.currentTable.map((column,i) => 
+                        <option value={column} key={i}>{column.column_name}</option>    
+                    )}
+                    </TextField>;
+                // if(this.state..length){
+                    
+                // }
+            } else if (this.state.modification === 'POST') {
 
             }
         }
@@ -85,6 +113,8 @@ class AddTarget extends Component {
                 <p>Target</p>
                 {modificationEl}
                 {tableEl}
+                {columnSelectEl}
+                {valueEl}
             </div>
             
         )
